d5cd616da1bc30248e9c1705f91ce4341f2e9fce
caio henrique <trolliama@pop-os.localdomain>
2021-09-24 18:06:29 -0300

    cast the django debug env variable to integer

--- app/app/settings.py
+++ app/app/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = os.environ.get("DJANGO_DEBUG", False)
+DEBUG = int(os.environ.get("DJANGO_DEBUG", 0))
 
 ALLOWED_HOSTS = []
 
